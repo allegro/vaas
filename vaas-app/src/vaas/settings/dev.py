fcc395f8647ee92377bbbc327d32a05dcff9418e
Szymon Jasinski <szymon.jasinski@allegro.pl>
2016-05-16 12:46:20 +0200

    Adding dev environment settings file

---
+++ vaas-app/src/vaas/settings/dev.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+from .base import *
+
+from vaas.configuration.loader import YamlConfigLoader
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+for key, value in YamlConfigLoader().get_config_tree('dev.yml').iteritems():
+    globals()[key] = value
