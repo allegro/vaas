1c9a1f587a417051837e35d8ac6c2353481100ac
Marcin Zaremba <marcin.zaremba@allegrogroup.com>
2016-05-19 17:54:20 +0200

    PLUTON-4632 | Add one line for filtering

--- vaas-app/src/vaas/manager/admin.py
+++ vaas-app/src/vaas/manager/admin.py
@@ -42,13 +42,14 @@ def enable_director(modeladmin, request, queryset):
 
 def disable_director(modeladmin, request, queryset):
     switch_state_and_reload(queryset, False)
-
+    
 
 class DirectorAdmin(admin.ModelAdmin):
     search_fields = ['name', 'route_expression']
     form = DirectorModelForm
     list_display = ('name', 'service', 'get_clusters', 'route_expression', 'probe', 'custom_enabled')
     list_filter = ['cluster__name']
+    filter_horizontal = ('cluster',)
     actions = [enable_director, disable_director]
 
     def get_clusters(self, obj):
