b9da80c9b3d99355f6c738cefd27abda7a44dff1
Tomasz Ziolkowski <tomasz.ziolkowski@allegrogroup.com>
2017-11-17 14:02:17 +0100

    check also plugins from INSTALLED_PLUGINS

--- plugins/urls.py
+++ plugins/urls.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from django.conf.urls import url, include
+from django.conf import settings
 
 
 logger = logging.getLogger(__name__)
@@ -10,7 +11,15 @@ urlpatterns = []
 
 PLUGIN_DIR = os.path.abspath(os.path.dirname(__file__))
 
-for app in next(os.walk(PLUGIN_DIR))[1]:
+
+def iterate_plugins():
+    for app in next(os.walk(PLUGIN_DIR))[1]:
+        yield app
+    for app in getattr(settings, 'INSTALLED_PLUGINS', []):
+        yield app
+
+
+for app in iterate_plugins():
     try:
         urlpatterns.append(url(r'{}/'.format(app), include('{}.urls'.format(app))))
         logger.info('Found urls for plugin: {}'.format(app))
