886189154ca31b69955dfbb956dd139d9c488811
Artur Mackowiak <artur.mackowiak@allegro.pl>
2019-05-31 13:10:47 +0200

    Add custom __str__ for Route

--- vaas-app/src/vaas/router/models.py
+++ vaas-app/src/vaas/router/models.py
@@ -17,5 +17,10 @@ class Route(models.Model):
     director = models.ForeignKey(Director, on_delete=models.PROTECT)
     action = models.CharField(max_length=20, choices=ACTION_CHOICES)
 
+    def __str__(self):
+        name = "(Priority: %s) if (req.url ~ %s) then %s for %s" % (
+            self.priority, self.condition, self.action, str(self.director))
+        return name
+
     class Meta:
         unique_together = (('priority', 'cluster', 'director'),)
