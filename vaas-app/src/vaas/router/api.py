894e5a9b96e59937a9770a8436674665e5184264
Maciej Nowakowski <maciej.nowakowski@allegro.pl>
2019-11-25 10:13:41 +0100

    Fix validation for api

--- vaas-app/src/vaas/router/api.py
+++ vaas-app/src/vaas/router/api.py
@@ -1,22 +1,14 @@
 # -*- coding: utf-8 -*-
-import logging
-
-from django.core.exceptions import ObjectDoesNotExist
-
 from tastypie.resources import ModelResource, ALL_WITH_RELATIONS, Resource
 from tastypie import fields
-from tastypie.exceptions import ApiFieldError
 from tastypie.authentication import ApiKeyAuthentication, MultiAuthentication, SessionAuthentication
 from vaas.external.tasty_validation import ModelCleanedDataFormValidation
 
 from vaas.external.api import ExtendedDjangoAuthorization as DjangoAuthorization
 from vaas.external.serializer import PrettyJSONSerializer
-from vaas.cluster.models import LogicalCluster
-from vaas.manager.models import Director
 from vaas.router.models import Route
 from vaas.router.forms import RouteModelForm
-
-logger = logging.getLogger('vaas')
+from vaas.adminext.widgets import split_condition
 
 
 class RouteResource(ModelResource):
@@ -36,25 +28,14 @@ class RouteResource(ModelResource):
             'clusters': ALL_WITH_RELATIONS
         }
 
+    def hydrate_condition(self, bundle):
+        bundle.data['condition_0'], bundle.data['condition_1'], bundle.data['condition_2'] = split_condition(
+            bundle.data['condition']
+        )
+        return bundle
+
     def dehydrate_director(self, bundle):
         return bundle.obj.director.name
 
     def dehydrate_clusters(self, bundle):
         return list(bundle.obj.clusters.values_list('name', flat=True))
-
-    def hydrate_director(self, bundle):
-        try:
-            bundle.data['director'] = Director.objects.get(name=bundle.data['director'])
-        except ObjectDoesNotExist:
-            logger.info("[RouteResource.hydrate_director()] provided name = %s", bundle.data['director'])
-            raise ApiFieldError("Could not find the provided director via resource name '%s'."
-                                % bundle.data['director'])
-        return bundle
-
-    def hydrate_clusters(self, bundle):
-        try:
-            bundle.data['clusters'] = LogicalCluster.objects.get(name__in=bundle.data['clusters'])
-        except ObjectDoesNotExist:
-            logger.info("[RouteResource.hydrate_clusters()] provided name = %s", bundle.data['clusters'])
-            raise ApiFieldError("Could not find the provided cluster via resource name '%s'." % bundle.data['clusters'])
-        return bundle
