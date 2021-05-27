6a6318f6e2c2a2fa9a3ff4054e3e49b21adb2f39
Artur Mackowiak <artur.mackowiak@allegro.pl>
2021-05-27 11:07:52 +0200

    Add default variables when import data

--- vaas-app/src/vaas/vcl/renderer.py
+++ vaas-app/src/vaas/vcl/renderer.py
@@ -293,7 +293,7 @@ class VclTagBuilder(object):
             vcl_tag.parameters['allow_metrics_header'] = settings.ALLOW_METRICS_HEADER
         elif tag_name in ('RECV'):
             vcl_tag.parameters['mesh_routing'] = self.placeholders['mesh_routing']
-        elif tag_name in ('ROUTER', 'EXPLICITE_ROUTER', 'DIRECTORS', 'RECV'):
+        elif tag_name in ('ROUTER', 'EXPLICITE_ROUTER', 'DIRECTORS'):
             vcl_tag.parameters['vcl_directors'] = self.placeholders['vcl_director']
             vcl_tag.parameters['directors'] = self.placeholders['active_director']
             vcl_tag.parameters['probes'] = self.placeholders['probe']
