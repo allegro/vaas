cc320d531725b356d3be312d78e9428faec1c455
Maciej <maciej.nowakowski@allegro.pl>
2022-01-27 10:19:45 +0100

    Fix spacing

--- vaas/vaas/monitor/tasks.py
+++ vaas/vaas/monitor/tasks.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from vaas.settings.celery import app
 from vaas.monitor.health import BackendStatusManager
 
-@app.task(bind=True,  soft_time_limit=settings.CELERY_TASK_SOFT_TIME_LIMIT_SECONDS)
+
+@app.task(bind=True, soft_time_limit=settings.CELERY_TASK_SOFT_TIME_LIMIT_SECONDS)
 def refresh_backend_statuses(self):
     BackendStatusManager().refresh_statuses()
