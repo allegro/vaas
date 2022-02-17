4d189e3b3cd0c04d801828f794db45a3ae92bb80
smigacz <michalsm@poczta.wp.pl>
2022-02-17 10:15:38 +0100

    chose logging formater

--- vaas/vaas/settings/production.py
+++ vaas/vaas/settings/production.py
@@ -14,6 +14,7 @@ for key, value in YamlConfigLoader(['/configuration']).get_config_tree('config.y
 
 INSTALLED_APPS = tuple(INSTALLED_PLUGINS) + INSTALLED_APPS
 MIDDLEWARE = MIDDLEWARE + list(MIDDLEWARE_PLUGINS)
+LOGGING['handlers']['console']['formatter'] = os.environ.get('CONSOLE_LOG_FORMATTER', 'verbose')
 
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
