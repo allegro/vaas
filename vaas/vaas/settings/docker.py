9378875b4fec95a709f8c3a7ff625cc27bb7e111
smigacz <michalsm@poczta.wp.pl>
2022-02-17 09:55:42 +0100

    logging on console for base settings

--- vaas/vaas/settings/docker.py
+++ vaas/vaas/settings/docker.py
@@ -11,6 +11,44 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = tuple(INSTALLED_PLUGINS) + INSTALLED_APPS
 MIDDLEWARE = MIDDLEWARE + list(MIDDLEWARE_PLUGINS)
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/debug.log',
+            'formatter': 'verbose'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file', 'console'],
+            'propagate': False,
+            'level': 'ERROR',
+        },
+        'vaas': {
+            'handlers': ['file', 'console'],
+            'propagate': False,
+            'level': 'DEBUG',
+        },
+        '': {
+            'handlers': ['file', 'console'],
+            'level': 'INFO',
+        }
+    }
+}
+
 for key, value in YamlConfigLoader(['/configuration']).get_config_tree('docker.yaml').items():
     globals()[key.upper()] = value
 
