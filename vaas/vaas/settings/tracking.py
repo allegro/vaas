c76cd07c39f228f4a2c6caa4435776ba007e043a
smigacz <michalsm@poczta.wp.pl>
2022-02-28 09:55:12 +0100

    add tracking settings

--- vaas/vaas/settings/tracking.py
+++ vaas/vaas/settings/tracking.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-  #Here you can add tools to track application performance
+    # Here you can add tools to track application performance
