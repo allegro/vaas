32f292de0483b5f365fa0ee54984ab28df868f0d
smigacz <michalsm@poczta.wp.pl>
2022-03-09 12:49:52 +0100

    read broker_url url for celery worker from config

--- vaas/vaas/settings/dev_celery_worker.py
+++ vaas/vaas/settings/dev_celery_worker.py
@@ -1,10 +1,11 @@
 from .dev import *
 
-# from vaas.configuration.loader import YamlConfigLoader
+from vaas.configuration.loader import YamlConfigLoader
 
 
-# if 'CELERY_WORKER_BROKER_URL_BASE' in globals():
-#     BROKER_URL = CELERY_WORKER_BROKER_URL_BASE
-#     CELERY_RESULT_BACKEND = CELERY_WORKER_RESULT_BACKEND_BASE
-BROKER_URL = 'redis://localhost:6379/1'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/2'
+for key, value in YamlConfigLoader(['/configuration']).get_config_tree('config.yaml').items():
+    globals()[key.upper()] = value
+
+if 'CELERY_WORKER_BROKER_URL_BASE' in globals():
+     BROKER_URL = CELERY_WORKER_BROKER_URL_BASE
+     CELERY_RESULT_BACKEND = CELERY_WORKER_RESULT_BACKEND_BASE
