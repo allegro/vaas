b1f19e7d9fc55417934b63063ec8a2e0a95a829a
smigacz <michalsm@poczta.wp.pl>
2022-03-10 13:34:29 +0100

    read globals on end of base settings

--- vaas/vaas/settings/oauth.py
+++ vaas/vaas/settings/oauth.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
+from .base import AUTHENTICATION_BACKENDS, SOCIAL_AUTH_PIPELINE
+
 AUTHENTICATION_BACKENDS = tuple(AUTHENTICATION_BACKENDS)
 SOCIAL_AUTH_PIPELINE = tuple(SOCIAL_AUTH_PIPELINE)
