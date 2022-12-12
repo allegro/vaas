8fe275928f74c58222d7fdcf6aa58f4527df1392
TheMickeyMike <maciej.nowakowski@allegro.pl>
2022-12-12 11:31:16 +0100

    Work2

--- vaas/vaas/router/static/js/redirect.js
+++ vaas/vaas/router/static/js/redirect.js
@@ -1,4 +1,74 @@
-const noDataPlaceholder = { name: 'no data', id: 'no data' }
+var RaportRenderer = function (ref, raport) {
+    this.ref = ref;
+    this.raport = raport;
+
+    this.show = function () { this.raport.render(this.ref); }
+};
+
+
+var Redirect = function (value) {
+    this.value = value;
+
+    this.createPanel = function (name, id, location, condition) {
+        return `
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h5><strong>Location:</strong> <code>${location || 'no data'}</code></h5>
+                    <h6 class="text-muted">redirect.contition: ${condition || 'no data'}</h6>
+                    <h6 class="text-muted">redirect.id: ${id || 'no data'}</h6>
+                </div>
+                <div class="panel-footer">${name}</div>
+            </div>
+        </div>`
+    }
+
+    this.sortByErrors = function () {
+        this.value.sort(function (x, y) {
+            if (x.error_message && !y.error_message) {
+                return -1;
+            }
+            if (!x.error_message && y.error_message) {
+                return 1;
+            }
+            if (x.expected.redirect.id < y.expected.redirect.id) {
+                return -1;
+            }
+            return 1;
+        });
+    }
+
+    this.renderError = function (error = '') {
+        if (error != '') {
+            return `<div class="alert alert-danger" role="alert">${error}</div>`
+        }
+        return error
+    }
+
+    this.render = function (ref) {
+        this.sortByErrors()
+        var html = '';
+        this.value.forEach(function (record) {
+            if (!record.current.redirect) record.current.redirect = { name: 'no data', id: 'no data' }
+            html += `<div class="panel panel-${statusToClass(record.result)}">
+                <div class="panel-heading">
+                <h3 class="panel-title">${record.url}</h3>
+                </div>
+                <div class="panel-body">
+                ${this.renderError(record.error_message)}
+                <div class="row">
+                    ${this.createPanel('Current', record.current.redirect.id, record.current.location, record.current.redirect.name)}
+                    ${this.createPanel('Excpeted', record.expected.redirect.id, record.expected.location, record.expected.redirect.name)}
+                </div>
+            </div>`
+        }, this);
+        ref.append(html);
+    }
+};
+
+
+
+
 function handleCommandValidateResponse(data, textStatus, request) {
     const status = data.status
     if (checkCommandStatus('done')) return;
@@ -9,7 +79,8 @@ function handleCommandValidateResponse(data, textStatus, request) {
     }
     else if (status === 'SUCCESS') {
         setCommandStatus('done');
-        $('#results').append(createTestResult(data.output.validation_results))
+        createTestResult(data.output.validation_results)
+        // $('#results').append(createTestResult(data.output.validation_results))
         $('#pass-count').text(countResult('PASS', data.output.validation_results));
         $('#failed-count').text(countResult('FAIL', data.output.validation_results));
         $('#command-result').addClass(`label-${statusToClass(data.output.validation_status)}`).text(data.output.validation_status);
@@ -20,59 +91,9 @@ function handleCommandValidateResponse(data, textStatus, request) {
 }
 
 function createTestResult(records) {
-    var html = '';
-    records.sort(function(x, y) {
-    if (x.error_message && !y.error_message) {
-    return -1;
-    }
-    if (!x.error_message && y.error_message) {
-    return 1;
-    }
-    if (x.expected.redirect.id < y.expected.redirect.id) {
-    return -1;
-    }
-    return 1;
-    });
-    records.forEach(function (record) {
-        var errorMsg = ''
-        if (!record.current.redirect) record.current.redirect = noDataPlaceholder
-        if (record.error_message) errorMsg = `<div class="alert alert-danger" role="alert">${record.error_message}</div>`
-        html += `<div class="panel panel-${statusToClass(record.result)}">
-            <div class="panel-heading">
-            <h3 class="panel-title">${record.url}</h3>
-            </div>
-            <div class="panel-body">
-            ${errorMsg}
-            <div class="row">
-                <div class="col-md-6">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                    <h5><strong>Location:</strong> <code>${record.current.location}</code></h5>
-                    <h6 class="text-muted">redirect.contition: ${record.current.redirect.name}</h6>
-                    <h6 class="text-muted">redirect.id: ${record.current.redirect.id}</h6>
-                    </div>
-                    <div class="panel-footer">
-                    Current
-                    </div>
-                </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="panel panel-default">
-                    <div class="panel-body">
-                        <h5><strong>Location:</strong> <code>${record.expected.location}</code></h5>
-                        <h6 class="text-muted">redirect.contition: ${record.expected.redirect.name}</h6>
-                        <h6 class="text-muted">redirect.id: ${record.expected.redirect.id}</h6>
-                    </div>
-                    <div class="panel-footer">
-                        Excpeted
-                    </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-        </div>`
-    });
-    return html
+    var redirect = new Redirect(records);
+    var raport = new RaportRenderer($('#results'), redirect)
+    raport.show();
 }
 
 function handleErrorResponse(request, textStatus, errorThrown) {
