7e029e2b5b7d4ccdb967c53d74464c5bb74279de
TheMickeyMike <maciej.nowakowski@allegro.pl>
2022-12-15 17:33:30 +0100

    Add rewrite groups field

--- vaas/vaas/router/models.py
+++ vaas/vaas/router/models.py
@@ -24,6 +24,7 @@ class Redirect(models.Model):
 
     src_domain = models.ForeignKey(DomainMapping, on_delete=models.PROTECT)
     condition = models.CharField(max_length=512)
+    rewrite_groups = models.CharField(max_length=512, default='', blank=True)
     destination = models.CharField(max_length=512, validators=[RegexValidator(
         regex="^/.*", message="Destination should be relative")])
     action = models.IntegerField(choices=ResponseStatusCode.choices, default=301)
