e52ece69664b13f632dd56913c2ab92a18684bc3
Artur <awmackowiak@gmail.com>
2023-08-28 13:06:26 +0200

    Refactor PrometheusClient

--- vaas/vaas/metrics/prometheus.py
+++ vaas/vaas/metrics/prometheus.py
@@ -1,24 +1,33 @@
 import logging
 
 from datetime import timedelta
-from typing import Optional, TypedDict, Union
+from typing import Dict, Optional, Protocol, Union
 from django.conf import settings
 from prometheus_client import CollectorRegistry, Gauge, Summary, push_to_gateway
+from prometheus_client.metrics import MetricWrapperBase
 
 from vaas.metrics.models import Metrics
 
 logger = logging.getLogger(__name__)
 
-class MetricsBucket(TypedDict, total=False):
-    name: str
-    metric: Union[Summary, Gauge]
-
+class CollectorProtocol(Protocol):
+    def __call__(self, name: str, documentation: str, registry: CollectorRegistry) -> MetricWrapperBase:
+        ...
 
 class PrometheusClient:
-    def __init__(self, registry: CollectorRegistry) -> None:
+    def __init__(self) -> None:
         self.host: str = f'{settings.PROMETHEUS_GATEWAY_HOST}:{settings.PROMETHEUS_GATEWAY_PORT}'
         self.job: str = settings.PROMETHEUS_GATEWAY_JOB
-        self.registry: CollectorRegistry = registry
+        self.metrics_bucket: Dict[str, CollectorProtocol] = {}
+        self.registry: CollectorRegistry = CollectorRegistry()
+
+    def get_or_create(self, name: str, type: CollectorProtocol) -> CollectorProtocol:
+        name_with_suffix: str = f'{name}_microseconds'
+        metric: Optional[CollectorProtocol] = self.metrics_bucket.get(name_with_suffix)
+        if not metric:
+            self.metrics_bucket[name_with_suffix] = type(name_with_suffix, name, registry=self.registry) # type: ignore
+            return self.metrics_bucket.get(name_with_suffix) # type: ignore
+        return metric
 
     def push(self) -> None:
         try:
@@ -30,25 +39,15 @@ class PrometheusClient:
 
 class PrometheusMetrics(Metrics):
     def __init__(self) -> None:
-        self.metrics_bucket: MetricsBucket = {}
-        self.registry: CollectorRegistry = CollectorRegistry()
         super().__init__(
-            client=PrometheusClient(self.registry))
-
-    def _get_or_create(self, name: str, type: Union[Summary, Gauge]) -> Optional[Union[Summary, Gauge]]:
-        name_with_suffix: str = f'{name}_microseconds'
-        metric = self.metrics_bucket.get(name_with_suffix)
-        if not metric:
-            self.metrics_bucket[name_with_suffix] = type(name_with_suffix, name, registry=self.registry) # type: ignore
-            return self.metrics_bucket.get(name_with_suffix)
-        return metric
+            client=PrometheusClient())
 
     def sum(self, metric_name: str, value: timedelta) -> None:
         if metric_name:
-            self._get_or_create(metric_name, Summary).observe(value.microseconds) # type: ignore
+            self.client.get_or_create(metric_name, Summary).observe(value.microseconds)
             self.client.push()
 
     def gauge(self, metric_name: str, value: Union[int, float]) -> None:
         if metric_name:
-            self._get_or_create(metric_name, Gauge).set(value) # type: ignore
+            self.client.get_or_create(metric_name, Gauge).set(value)
             self.client.push()
