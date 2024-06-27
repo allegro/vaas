6e7c488b32c3dcc9b383531214a6ae57bd9fdea3
Artur Maćkowiak <awmackowiak@gmail.com>
2024-06-27 21:18:33 +0200

    POC rebbitmq setup for testimg

--- vaas/vaas/settings/celery.py
+++ vaas/vaas/settings/celery.py
@@ -18,28 +18,28 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
-app.conf.beatx_store = settings.BROKER_URL
-app.conf.beat_max_loop_interval = settings.CELERY_BEAT_MAX_LOOP_INTERVAL
-app.conf.beat_schedule = {
-    'refresh_backend_statuses': {
-        'task': 'vaas.monitor.tasks.refresh_backend_statuses',
-        'schedule': settings.BACKEND_STATUSES_UPDATE_INTERVAL_SECONDS,
-    },
-}
+# app.conf.beatx_store = settings.BROKER_URL
+# app.conf.beat_max_loop_interval = settings.CELERY_BEAT_MAX_LOOP_INTERVAL
+# app.conf.beat_schedule = {
+#     'refresh_backend_statuses': {
+#         'task': 'vaas.monitor.tasks.refresh_backend_statuses',
+#         'schedule': settings.BACKEND_STATUSES_UPDATE_INTERVAL_SECONDS,
+#     },
+# }
 
-# After redis conenction troubles 'Connection closed by server / Connection by peer' we allow to re-queued
-# task which was executed when failure occurred. "We know what we are doing."
-# https://docs.celeryq.dev/en/latest/userguide/configuration.html#task-reject-on-worker-lost
-app.conf.task_reject_on_worker_lost = settings.CELERY_TASK_REJECT_ON_WORKER_LOST
+# # After redis conenction troubles 'Connection closed by server / Connection by peer' we allow to re-queued
+# # task which was executed when failure occurred. "We know what we are doing."
+# # https://docs.celeryq.dev/en/latest/userguide/configuration.html#task-reject-on-worker-lost
+# app.conf.task_reject_on_worker_lost = settings.CELERY_TASK_REJECT_ON_WORKER_LOST
 
-# For better handle redis ConenctionError exception we give possibility to configure keepalive and connect_timeout parameters
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-socket-keepalive
-app.conf.redis_socket_keepalive = settings.REDIS_SOCKET_KEEPALIVE
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-retry-on-timeout
-app.conf.redis_retry_on_timeout = settings.REDIS_RETRY_ON_TIMEOUT
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-socket-connect-timeout
-app.conf.redis_socket_connect_timeout = settings.REDIS_SOCKET_CONNECT_TIMEOUT
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-socket-timeout
-app.conf.redis_socket_timeout = settings.REDIS_SOCKET_TIMEOUT
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html?highlight=redis_retry_on_timeout#redis-backend-health-check-interval
-app.conf.redis_backend_health_check_interval = settings.REDIS_BACKEND_HEALTH_CHECK_INTERVAL_SEC
+# # For better handle redis ConenctionError exception we give possibility to configure keepalive and connect_timeout parameters
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-socket-keepalive
+# app.conf.redis_socket_keepalive = settings.REDIS_SOCKET_KEEPALIVE
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-retry-on-timeout
+# app.conf.redis_retry_on_timeout = settings.REDIS_RETRY_ON_TIMEOUT
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-socket-connect-timeout
+# app.conf.redis_socket_connect_timeout = settings.REDIS_SOCKET_CONNECT_TIMEOUT
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#redis-socket-timeout
+# app.conf.redis_socket_timeout = settings.REDIS_SOCKET_TIMEOUT
+# # https://docs.celeryq.dev/en/stable/userguide/configuration.html?highlight=redis_retry_on_timeout#redis-backend-health-check-interval
+# app.conf.redis_backend_health_check_interval = settings.REDIS_BACKEND_HEALTH_CHECK_INTERVAL_SEC
