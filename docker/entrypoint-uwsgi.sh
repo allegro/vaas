e0806dfc484f990c0509758a7ad2e9c36b88228c
Ryszard Dotka <ryszard.dotka@allegro.com>
2025-08-21 10:24:56 +0200

    Revert test.txt -> base.txt

--- docker/entrypoint-uwsgi.sh
+++ docker/entrypoint-uwsgi.sh
@@ -10,7 +10,7 @@ echo "Loading base-data"
 python /home/app/vaas/manage.py loaddata ./vaas/resources/base-data.yaml
 
 echo "Install the test requirements to allow [manage.py test] to be called within the container"
-pip install -r /home/app/vaas/requirements/test.txt
+pip install -r /home/app/vaas/requirements/base.txt
 
 echo "Start uwsgi server"
 exec uwsgi --ini /etc/uwsgi.cfg --http-socket :3030
