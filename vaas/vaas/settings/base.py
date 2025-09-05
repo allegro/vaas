4623f409746d7f57a370f0904f0e9656770b7a01
Artur <awmackowiak@gmail.com>
2025-09-05 08:42:44 +0200

    Working modal

--- vaas/vaas/settings/base.py
+++ vaas/vaas/settings/base.py
@@ -461,7 +461,7 @@ JAZZMIN_SETTINGS = {
     # UI Tweaks #
     #############
     # Relative paths to custom CSS/JS scripts (must be present in static files)
-    "custom_css": "custom.css",
+    "custom_css": "modal.css",
     "custom_js": None,
     # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
     "use_google_fonts_cdn": True,
